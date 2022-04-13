f9c82a981e3263d0649896ae4b55f4e86abb559a
profitsw2000 <profitsw2000@gmail.com>
2022-04-13 15:19:45 +0300

    Создан адаптер для списка пользователей.

---
+++ app/src/main/java/ru/profitsw2000/githubclient/UserListAdapter.kt
@@ -0,0 +1,24 @@
+package ru.profitsw2000.githubclient
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UserListAdapter: RecyclerView.Adapter<UserListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
