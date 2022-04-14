d54d6cd4e2712d2efcafbc10cde5a2aaf4d24903
profitsw2000 <profitsw2000@gmail.com>
2022-04-14 15:38:04 +0300

    Добавлены классы ClientApiUseCaseImpl.kt и TestClientApiImpl.kt с реализациями интерфейсов ClientApiUseCase.kt и ClientApi.kt.

---
+++ app/src/main/java/ru/profitsw2000/githubclient/data/ClientApiUseCaseImpl.kt
@@ -0,0 +1,21 @@
+package ru.profitsw2000.githubclient.data
+
+import android.os.Handler
+import androidx.annotation.MainThread
+import ru.profitsw2000.githubclient.domain.ClientApi
+import ru.profitsw2000.githubclient.domain.ClientApiUseCase
+import ru.profitsw2000.githubclient.domain.entities.UserProfile
+
+class ClientApiUseCaseImpl(
+    private val api: ClientApi,
+    private val uiHandler: Handler
+    ) : ClientApiUseCase {
+    override fun getUserList(@MainThread callback: (List<UserProfile>) -> Unit) {
+        Thread {
+            val result = api.getUserList()
+            uiHandler.post {
+                callback(result)
+            }
+        }.start()
+    }
+}
---
+++ app/src/main/java/ru/profitsw2000/githubclient/data/TestClientApiImpl.kt
@@ -0,0 +1,47 @@
+package ru.profitsw2000.githubclient.data
+
+import ru.profitsw2000.githubclient.data.repository.LocalRepoImpl
+import ru.profitsw2000.githubclient.domain.ClientApi
+import ru.profitsw2000.githubclient.domain.entities.UserProfile
+
+class TestClientApiImpl : ClientApi {
+    private val localRepo = LocalRepoImpl()
+
+    override fun getUserList(): List<UserProfile> {
+        Thread.sleep(2000)
+        return localRepo.getAllUsers()
+    }
+
+    override fun createNewUser(
+        name: String,
+        info: String,
+        city: String,
+        avatarUrl: String
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeUserInfo(id: Int, info: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeUserCity(id: Int, info: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeUserAvatar(info: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun createUserRepository(userRepository: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteUserRepository(userRepository: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteUserFromDatabase(id: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
