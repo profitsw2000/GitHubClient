97d0d9f697364b6f8a949b361eb3f653ba800f07
profitsw2000 <profitsw2000@gmail.com>
2022-04-15 11:04:57 +0300

    Создан адаптер и макет элемента для отображения списка репозиториев пользователя GitHub.

---
+++ app/src/main/java/ru/profitsw2000/githubclient/ui/screens/details/UserRepositoriesAdapter.kt
@@ -0,0 +1,25 @@
+package ru.profitsw2000.githubclient.ui.screens.details
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UserRepositoriesAdapter : RecyclerView.Adapter<UserRepositoriesAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): UserRepositoriesAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserRepositoriesAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
+}
