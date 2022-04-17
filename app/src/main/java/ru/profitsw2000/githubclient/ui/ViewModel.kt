16f25bd4ff0c2c1a2cff72bf8c84c095522e000e
profitsw2000 <profitsw2000@gmail.com>
2022-04-17 13:24:12 +0300

    Во ViewModel.kt и его реализацию MainViewModel.kt добавлена функция для загрузки списка пользователей GitHub с помощью RxJava и Retrofit.

--- app/src/main/java/ru/profitsw2000/githubclient/ui/ViewModel.kt
+++ app/src/main/java/ru/profitsw2000/githubclient/ui/ViewModel.kt
@@ -1,15 +1,19 @@
 package ru.profitsw2000.githubclient.ui
 
 import androidx.annotation.MainThread
+import ru.profitsw2000.githubclient.domain.entities.User
 import ru.profitsw2000.githubclient.domain.entities.UserProfile
 import ru.profitsw2000.githubclient.utils.Publisher
 
 interface ViewModel {
     val showProgress: Publisher<Boolean>
     val getUserProfileList: Publisher<List<UserProfile>>
+    val getUserList: Publisher<List<User>>
     val errorCode: Publisher<Int?>
 
     @MainThread
     fun onLoadUserList()
+
+    fun onLoadRxUserList()
 
 }
--- app/src/main/java/ru/profitsw2000/githubclient/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/ru/profitsw2000/githubclient/ui/screens/main/MainViewModel.kt
@@ -1,6 +1,9 @@
 package ru.profitsw2000.githubclient.ui.screens.main
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import ru.profitsw2000.githubclient.domain.ClientApiUseCase
+import ru.profitsw2000.githubclient.domain.entities.User
 import ru.profitsw2000.githubclient.domain.entities.UserProfile
 import ru.profitsw2000.githubclient.ui.ViewModel
 import ru.profitsw2000.githubclient.utils.Publisher
@@ -11,7 +14,10 @@ private const val ERROR_EMPTY_USERS_LIST = 1
 class MainViewModel (private val clientApiUseCase: ClientApiUseCase) : ViewModel {
     override val showProgress: Publisher<Boolean> = Publisher()
     override val getUserProfileList: Publisher<List<UserProfile>> = Publisher()
+    override val getUserList: Publisher<List<User>> = Publisher()
     override val errorCode: Publisher<Int?> = Publisher()
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onLoadUserList() {
         showProgress.post(true)
@@ -23,5 +29,16 @@ class MainViewModel (private val clientApiUseCase: ClientApiUseCase) : ViewModel
             }
             showProgress.post(false)
         }
+    }
+
+    override fun onLoadRxUserList() {
+        showProgress.post(true)
+        compositeDisposable.add(
+            clientApiUseCase.getRxUserList()
+                .subscribeBy {
+                    showProgress.post(false)
+                    getUserList.post(it)
+                }
+        )
     }
 }
