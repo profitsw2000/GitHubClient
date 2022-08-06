a0987a3240da8d686204f8f88f1af281051b04c7
profitsw2000 <profitsw2000@gmail.com>
2022-08-06 21:08:43 +0300

    Сделаны Unit-тесты для класса MockRepositoryImpl.kt.

---
+++ app/src/main/java/ru/profitsw2000/githubclient/domain/MockRepository.kt
@@ -0,0 +1,8 @@
+package ru.profitsw2000.githubclient.domain
+
+import androidx.annotation.MainThread
+import ru.profitsw2000.githubclient.data.local.entities.UserProfile
+
+interface MockRepository : RepositoryUseCase {
+    fun getUserList(): List<UserProfile>
+}
